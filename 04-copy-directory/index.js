003536c1e0005e425b11b980cf7780c7771824ed
grishanova-oa <grishanova.oa@gmail.com>
2022-11-04 13:40:53 +0100

    feat: done task 4

--- 04-copy-directory/index.js
+++ 04-copy-directory/index.js
@@ -1,1 +1,34 @@
 
+
+const fs = require('fs');   
+const path = require('path'); 
+const newStream = fs.createReadStream(__filename);
+const filesCopy = path.join(__dirname, 'files-copy');
+const filesName = path.join(__dirname, 'files');
+const promises = fs.promises;
+
+newStream.on('open', function createFolder() {
+
+    fs.stat(filesCopy, (err, stats) => {
+        if (!err) {
+            fs.rm(filesCopy, { recursive: true }, (err) => {
+                if (err) throw err;
+                return createFolder();                             
+            });
+        }
+        else {
+            promises.mkdir(filesCopy).then(() => {            
+                fs.readdir(filesName, (err, files) => {     
+                    if (err) throw err;
+                    files.forEach(elem => {
+                        fs.copyFile(path.join(filesName, `${elem}`), (path.join(filesCopy, `${elem}`)), (err) => {
+                            if (err) throw err;
+                        });
+                    })
+                });
+            }).catch( () => {
+                console.log('Directory  is not created');
+            });
+        }
+    });
+});
